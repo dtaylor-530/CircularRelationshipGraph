aeea72816000c4712ce5aafeac4fcc356a75fc55
dtaylor-530 <d.taylor530@gmail.com>
2022-09-18 10:34:20 +0100

    Moves ViewModel to new file

--- CircularRelationshipGraph.WPF.Infrastructure/ViewModel/NodeSegmentViewModel.cs
+++ CircularRelationshipGraph.WPF.Infrastructure/ViewModel/NodeSegmentViewModel.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace CircularRelationshipGraph
@@ -183,28 +181,5 @@ namespace CircularRelationshipGraph
                 OnPropertyChanged();
             }
         }
-    }
-
-    public class ViewModel : INotifyPropertyChanged
-    {
-        #region INotifyPropertyChanged Members
-
-        /// <summary>
-        /// Occurs when a property changes
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// Raises a PropertyChanged event
-        /// </summary>
-        protected void OnPropertyChanged([CallerMemberName] string? property = null)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(property));
-            }
-        }
-
-        #endregion INotifyPropertyChanged Members
     }
 }
---
+++ CircularRelationshipGraph.WPF.Infrastructure/ViewModel/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CircularRelationshipGraph
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+
+        /// <summary>
+        /// Occurs when a property changes
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises a PropertyChanged event
+        /// </summary>
+        protected void OnPropertyChanged([CallerMemberName] string? property = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        #endregion INotifyPropertyChanged Members
+    }
+}
